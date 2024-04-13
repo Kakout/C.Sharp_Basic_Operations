e5dc8d9da5461f1e855102dde8b2d40867c3a454
PieckFingerr <vinhgiachau@gmail.com>
2024-04-13 21:45:38 +0700

    Add project files.

---
+++ C#_Basic_Operations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C#_Basic_Operations 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
